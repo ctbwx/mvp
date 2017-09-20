4e6571f4d3801552a5f659e2da9fe4996121d397
Charlie <ctbwilhite@gmail.com>
2017-09-20 14:38:09 -0500

    Added forever as a dependancy

--- client/components/ImageBox.jsx
+++ client/components/ImageBox.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const ImageBox = ({image}) => (
+  !image ? <div className="imageBox">Please wait...</div> :
   <div className="imageBox">
     <img src={`${image}`} height="320" width="480"/>
   </div>
